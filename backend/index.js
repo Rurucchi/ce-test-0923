2e6206fe1c984e398acf89593a28b172d7c049e5
Rurucchi <rurucchi.pro@gmail.com>
2023-05-18 10:49:32 +0200

    sequelize models, might change for front-end?

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,17 @@ const cors = require("cors");
 
 // routes
 const starRouter = require("./star");
+
+// middleware logs in console
+app.use((req, res, next) => {
+  console.log(`Time: ${Date.now()} - ${req.method}: ${req.originalUrl}`);
+  next();
+});
+
+// routing middleware
+app.use("/star", starRouter, cors());
+
+// app listening
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
---
+++ backend/models.js
@@ -0,0 +1,11 @@
+import { Sequelize, Model, DataTypes } from "sequelize";
+
+const sequelize = new Sequelize("sqlite::memory:");
+
+const Student = sequelize.define("Student", {
+  idStudent: DataTypes.NUMBER,
+});
+
+const Mentor = sequelize.define("Mentor", {
+  idMentor: DataTypes.NUMBER,
+});
