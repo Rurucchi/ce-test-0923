1300096227bab24f8ba53b0771f8268b38ce8e75
Rurucchi <rurucchi.pro@gmail.com>
2023-05-18 10:30:02 +0200

    backend : project init, basic file structure

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+// packages needed
+const express = require("express");
+const app = express();
+const port = 4000;
+const cors = require("cors");
+
+// routes
+const starRouter = require("./star");
